d2e96d0d0d08c09fb93a966ae8aed659651ff360
milanwiedemann <milan.wiedemann@gmail.com>
2023-01-16 15:34:06 +0200

    Add helper methods for DM variables to Dataset
    This adds comments from Peter.
    Ideally I'd like to have one subclass for all variables `DmDatasetVariables` and one for all business rules `DmDatasetRules` if possible.

--- analysis/dataset_definition_dm017.py
+++ analysis/dataset_definition_dm017.py
@@ -1,12 +1,9 @@
 from datetime import date, timedelta
 
 from variable_lib_dm import (
+    DmDataset,
     get_has_not_died,
-    dataset,
     get_gms_registration_status,
-    add_diabetes_mellitus_register_variables,
-    get_dm_reg_r1,
-    get_dm_reg_r2,
 )
 
 # Define index date
@@ -17,18 +14,20 @@ index_date = date(2022, 3, 31)
 has_not_died = get_has_not_died(index_date)
 gms_registration_status = get_gms_registration_status(index_date)
 
-# Create dataset and add diabetes register variables
-dataset = add_diabetes_mellitus_register_variables(dataset, index_date)
+# Create dataset and add diabetes mellitus variables
+dataset = DmDataset()
+dataset.add_diabetes_mellitus_variables(index_date)
+
 
 ########################################################
 # Define Business Rules for Diabetes register (RM_REG) #
 ########################################################
 
 # DM_REG rule 1:
-dataset.dm_reg_r1 = get_dm_reg_r1()
+dataset.dm_reg_r1 = dataset.get_dm_reg_r1()
 
 # DM_REG rule 2:
-dataset.dm_reg_r2 = get_dm_reg_r2()
+dataset.dm_reg_r2 = dataset.get_dm_reg_r2()
 
 dm_reg_select_r2 = dataset.dm_reg_r1 & ~dataset.dm_reg_r2
 
--- analysis/dataset_definition_dm020.py
+++ analysis/dataset_definition_dm020.py
@@ -1,22 +1,9 @@
 from datetime import date, timedelta
 
 from variable_lib_dm import (
+    DmDataset,
     get_has_not_died,
-    dataset,
     get_gms_registration_status,
-    add_diabetes_mellitus_register_variables,
-    get_dm_reg_r1,
-    get_dm_reg_r2,
-    get_dm020_dm021_r1,
-    get_dm020_dm021_r2,
-    get_dm020_dm021_r3,
-    get_dm020_dm021_r4,
-    get_dm020_dm021_r5,
-    get_dm020_dm021_r6,
-    get_dm020_dm021_r7,
-    get_dm020_dm021_r8,
-    get_dm020_dm021_r9,
-    get_dm020_dm021_r10,
 )
 
 # Define index date
@@ -27,44 +14,45 @@ index_date = date(2022, 3, 31)
 has_not_died = get_has_not_died(index_date)
 gms_registration_status = get_gms_registration_status(index_date)
 
-# Create dataset and add diabetes register variables
-dataset = add_diabetes_mellitus_register_variables(dataset, index_date)
+# Create dataset and add diabetes mellitus variables
+dataset = DmDataset()
+dataset.add_diabetes_mellitus_variables(index_date)
 
 # DM_REG rule 1:
-dataset.dm_reg_r1 = get_dm_reg_r1()
+dataset.dm_reg_r1 = dataset.get_dm_reg_r1()
 
 # DM_REG rule 2:
-dataset.dm_reg_r2 = get_dm_reg_r2()
+dataset.dm_reg_r2 = dataset.get_dm_reg_r2()
 
 # DM020 rule 1:
-dataset.dm020_r1 = get_dm020_dm021_r1()
+dataset.dm020_r1 = dataset.get_dm020_dm021_r1()
 
 # DM020 rule 2:
-dataset.dm020_r2 = get_dm020_dm021_r2(58.0, index_date)
+dataset.dm020_r2 = dataset.get_dm020_dm021_r2(58.0, index_date)
 
 # DM020 rule 3:
-dataset.dm020_r3 = get_dm020_dm021_r3(index_date)
+dataset.dm020_r3 = dataset.get_dm020_dm021_r3(index_date)
 
 # DM020 rule 4:
-dataset.dm020_r4 = get_dm020_dm021_r4(index_date)
+dataset.dm020_r4 = dataset.get_dm020_dm021_r4(index_date)
 
 # DM020 rule 5:
-dataset.dm020_r5 = get_dm020_dm021_r5(index_date)
+dataset.dm020_r5 = dataset.get_dm020_dm021_r5(index_date)
 
 # DM020 rule 6:
-dataset.dm020_r6 = get_dm020_dm021_r6(index_date)
+dataset.dm020_r6 = dataset.get_dm020_dm021_r6(index_date)
 
 # DM020 rule 7:
-dataset.dm020_r7 = get_dm020_dm021_r7(index_date)
+dataset.dm020_r7 = dataset.get_dm020_dm021_r7(index_date)
 
 # DM020 rule 8:
-dataset.dm020_r8 = get_dm020_dm021_r8(58.0, index_date)
+dataset.dm020_r8 = dataset.get_dm020_dm021_r8(58.0, index_date)
 
 # DM020 rule 9:
-dataset.dm020_r9 = get_dm020_dm021_r9(index_date)
+dataset.dm020_r9 = dataset.get_dm020_dm021_r9(index_date)
 
 # DM020 rule 10:
-dataset.dm020_r10 = get_dm020_dm021_r10(index_date)
+dataset.dm020_r10 = dataset.get_dm020_dm021_r10(index_date)
 
 # Define select action for DM_REG
 dm_reg_select_r2 = dataset.dm_reg_r1 & ~dataset.dm_reg_r2
